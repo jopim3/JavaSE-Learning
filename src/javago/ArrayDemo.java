2afd1a3db42c68f89f0c0727d5636f9d15d6af31
jopim3 <2540411930@qq.com>
2026-06-13 16:43:41 +0800

    一维数组

---
+++ src/javago/ArrayDemo.java
@@ -0,0 +1,10 @@
+package javago;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,3,4,5};
+        for (int i = 0;i<arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
